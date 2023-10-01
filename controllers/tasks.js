ac7a81d92a18e7bbd6ecf29854494574a0bb3cfe
Ronaldo Jara <sergiojara0609@gmail.com>
2023-10-01 18:29:06 -0500

    feat(04-ejercicio): create my first MVC API ✨

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,16 +1,31 @@
 // importar modelo
+import { TaskModel } from '../models/task.js'
 
-export const getAllTasks = (req, res) => {
-  const tasks = // ????
-  res.json(tasks)
+export const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await TaskModel.getAllTasks()
+    res.json(tasks)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
-export const addTask = (req, res) => {
-  const task = // ????
-  res.json(task)
+export const addTask = async (req, res) => {
+  try {
+    const { description } = req.body
+    const task = await TaskModel.addTask({ description })
+    res.status(200).json(task)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
-export const deleteTask = (req, res) => {
-  // ????
-  res.json({ message: 'Deleted successfully' })
+export const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params
+    await TaskModel.deleteTask({ id })
+    res.json({ message: 'Deleted successfully' })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
--- models/task.js
+++ models/task.js
@@ -15,28 +15,72 @@ let currentId = 1
  * @class
  */
 
-/**
- * export class TaskModel {
- *
- *
+export class TaskModel {
   /**
    * Fetches all tasks.
    * @name TaskModel.getAllTasks
    * @static
    * @returns {Array<Object>} Array of tasks.
    */
+  static async getAllTasks () {
+    return tasks
+  }
 
-/**
+  /**
    * Adds a new task to the list.
    * @name TaskModel.addTask
    * @static
    * @param {string} description - The description of the task.
    * @returns {Object} The new task object.
    */
+  static async addTask ({ description }) {
+    const newTask = {
+      id: currentId,
+      description
+    }
+    tasks.push(newTask)
+    ++currentId
+    return newTask
+  }
 
-/**
+  /**
    * Deletes a task from the list by its ID.
    * @name TaskModel.deleteTask
    * @static
    * @param {number} id - The ID of the task to delete.
    */
+  static async deleteTask ({ id }) {
+    const taskIndex = tasks.findIndex((task) => {
+      const parsedId = task.id.toString()
+      return parsedId === id
+    })
+    if (taskIndex === -1) return
+    tasks.splice(taskIndex, 1)
+    return id
+  }
+}
+
+/*
+ @midudev Hola midu espero hayas tenido un buen viaje y la hayas pasado genial en Argentina 🇦🇷❤️,
+ gracias a que revise el código de mis compañeros encontre esto en el código de @madeval 💜:
+
+ export class TaskNotFoundError extends Error {
+    constructor(message) {
+      super(message)
+      this.name = 'TaskNotFoundError'
+    }
+  }
+
+ y me recorde que hace tiempo enseñaste como podemos crear nuestros propios errores y manejarlos,
+ y mencionaste que es bueno manejar los errores en cada modelo y al ver esto me pregunte si seria buena
+ idea si usamos estos "custom errors" junto con un trycatch en cada controlador, algo como esto:
+
+ `res.status(error.status || 500).json({ message: error.message })")`
+
+ Con el fin de que automaticamente al lanzar el error tome tanto el mensaje como el status del
+ "custom error", seria bueno idea hacer esto o seria malo tener un trycatch todo poderoso ?
+ Hay mejores formas de controlar errores y si lo hay cuales serian ? Como se pueden aprovechar estos
+ "custom errors" en express ?
+
+ Gracias por todo midu, te deseo un excelente día !! ✨
+*/
